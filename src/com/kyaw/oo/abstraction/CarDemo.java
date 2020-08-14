9fabc898ae9779c8b7bc585c97bdde32cba1c3a1
kyaw-codes <kyaw.codes@gmail.com>
2020-08-14 22:50:36 +0630

    Clean up

--- src/com/kyaw/oo/abstraction/CarDemo.java
+++ src/com/kyaw/oo/abstraction/CarDemo.java
@@ -5,34 +5,16 @@ public class CarDemo {
     public static void main(String[] args) {
 
         ManualCar manualCar = new Lamborghini("L-1001");
-
         manualCar.changeGear(ManualGearbox.ONE);
-
-
         manualCar.changeGear(ManualGearbox.TWO);
-
-
         manualCar.pressCarHorn();
-
-
         manualCar.drive();
-
-
         manualCar.changeGear(ManualGearbox.THREE);
 
-
-
-        AutoGearCar gearCar = new Toyota("Honda 2014");
-
-        gearCar.drive();
-
-
-        gearCar.pressCarHorn();
-
-
-        gearCar.driveWithAutoPilot();
-
-
+        AutoGearCar autoGearCar = new Toyota("Honda 2014");
+        autoGearCar.drive();
+        autoGearCar.pressCarHorn();
+        autoGearCar.driveWithAutoPilot();
 
     }
 }
--- src/com/kyaw/oo/inheritance/Zoo.java
+++ src/com/kyaw/oo/inheritance/Zoo.java
@@ -5,21 +5,15 @@ public class Zoo {
     public static void main(String[] args) {
 
         Mammal monkey = new Monkey();
-
         monkey.eat();
-
         monkey.run();
 
         Monkey castedMonkey = (Monkey) monkey;
-
         castedMonkey.setColor("white");
-
         castedMonkey.dance();
 
         Mammal tiger = new Tiger();
-
         tiger.eat();
-
         tiger.run();
 
     }
